3ed74146442bc92f8469186e781a7c12211e648d
Adolphsa <497749990@qq.com>
2019-09-25 17:03:34 +0800

    Initial commit

---
+++ app/src/main/java/com/saina/uitestdemo/Ball.java
@@ -0,0 +1,22 @@
+package com.saina.uitestdemo;
+
+/**
+ * @author lucas
+ * @package com.saina.uitestdemo
+ * @fileName Ball
+ * @date on 2019/9/19 9:21
+ * @describe 粒子封装对象
+ */
+public class Ball {
+
+    public int color; //图片像素点颜色值
+    public float x; //粒子圆心坐标x
+    public float y; //粒子圆心坐标y
+    public float r; //粒子半径
+
+    public float vX;//粒子运动水平方向速度
+    public float vY;//粒子运动垂直方向速度
+    public float aX;//粒子运动水平方向加速度
+    public float aY;//粒子运动垂直方向加速度
+
+}
